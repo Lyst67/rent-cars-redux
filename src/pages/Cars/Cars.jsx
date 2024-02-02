046e71bc9ac6b3fcd271076b42b6e378979f7fa4
Lyst67 <p_listopad@ukr.net>
2024-02-02 16:08:36 +0200

    11

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -2,17 +2,17 @@ import { ChooseForm } from 'components/ChooseForm/ChooseForm';
 import css from './Cars.module.css';
 import { CarList } from 'components/CarList/CarList';
 
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchCars } from 'redux/cars/operations';
+// import { useDispatch } from 'react-redux';
+// import { useEffect } from 'react';
+// import { fetchCars } from 'redux/cars/operations';
 
 const cars = [{ a: '1' }, { b: '2' }, { c: '3' }];
 
 const Cars = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchCars());
-  }, [dispatch]);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   dispatch(fetchCars());
+  // }, [dispatch]);
   return (
     <section>
       <div className={css.cars_container}>
