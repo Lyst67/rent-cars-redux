c51d3b9063485ea2949f18ef29240b8ce15cadbd
Lyst67 <p_listopad@ukr.net>
2024-02-05 11:15:19 +0200

    favorite

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -3,14 +3,24 @@ import css from './Favorite.module.css';
 import { CarList } from 'components/CarList/CarList';
 import { useSelector } from 'react-redux';
 import { selectFavorites } from 'app/features/cars/selectors';
+import { useState } from 'react';
 
 function Favorite() {
   const cars = useSelector(selectFavorites);
+  const [selectedBrand, setSelectedBrand] = useState(null);
+
+  const filteredCars = selectedBrand
+    ? cars.filter(car => car.make.toLowerCase() === selectedBrand)
+    : cars;
+
+  function handleChooseMake(brand) {
+    setSelectedBrand(brand);
+  }
   return (
     <section className={css.section}>
       <div className={css.favorite_container}>
-        <ChooseForm />
-        <CarList cars={cars} />
+        <ChooseForm onChange={handleChooseMake} />
+        <CarList cars={filteredCars} />
       </div>
     </section>
   );
